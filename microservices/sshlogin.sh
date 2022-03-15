d7fbb3749235df75b263707efbbae68ef450222c
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-14 23:43:19 -0400

    1

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13 << EOF
+sshpass -p '2446592ny' ssh jianhe@192.168.2.4 << EOF
 	cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     bash update_containers.sh $1 4
 EOF
