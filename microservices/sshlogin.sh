619289f0292d39edb286f92d83c90d02be41dc97
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-15 00:43:37 -0400

    Update sshlogin.sh

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 sshpass -p '2446592ny' ssh jianhe@192.168.2.13<< EOF
 	cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
+    docker login
     docker-compose pull
     bash update_containers.sh $1 4
 EOF
