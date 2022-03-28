f940106ba4ca3a28b3b2cec9fc525ebd48c3de9f
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-28 00:01:18 -0400

    1

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -2,9 +2,5 @@
 sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     security -v unlock-keychain -p "2446592ny" ~/Library/Keychains/login.keychain-db
-    # git fetch --all
-    # git checkout origin/master -- /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/docker-compose.yml
-    # docker pull jh7939/microservices:$1_microservice
-    # bash add_newservice.sh $1
     bash update_containers.sh $1 4
 EOF
