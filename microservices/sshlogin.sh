5b65de84520d82ea27711a8baacff901fcabd018
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-28 01:58:00 -0400

    1

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -2,9 +2,9 @@
 sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     security -v unlock-keychain -p "2446592ny" ~/Library/Keychains/login.keychain-db
-    IFS=','  read -R FOLDERS <<< $1  
-    for i in "${FOLDERS[@]}"; do  
-        echo "$i"
+    IFS=','  read -A FOLDERS <<< $1  
+    for i in $FOLDERS[@]; do  
+        echo i
         echo "hello"
     done
     # git fetch --all
