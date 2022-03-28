911405376ac2273cf8d1d29646893e03a669004a
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-28 02:20:29 -0400

    1

--- microservices/helper.sh
+++ microservices/helper.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-text=$(<list.txt)
-lastElement=${text##*-}
-output=""
-while IFS= read -r name 
-do
-  output="$name$output"
-done<<<$lastElement
-echo "$lastElement"
+#!/usr/bin/bash
+cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
+git fetch --all
+git checkout origin/master -- /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/docker-compose.yml
+IFS=', '  read -ra FOLDERS <<< $1  
+for i in "${FOLDERS[@]}"; do
+    docker pull jh7939/microservices:${i}_microservice
+    bash add_newservice.sh $i
+    bash update_containers.sh $i 4
+done
--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -2,6 +2,6 @@
 sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     security -v unlock-keychain -p "2446592ny" ~/Library/Keychains/login.keychain-db
-    bash test.sh $1
+    bash helper.sh $1
     exit
 EOF
