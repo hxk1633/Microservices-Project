305aaa71818881f225368e2370884d018478e1fa
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-28 01:22:24 -0400

    1

--- monolithic-app/sshlogin.sh
+++ monolithic-app/sshlogin.sh
@@ -6,6 +6,6 @@ sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     git fetch --all
     git checkout origin/master -- /Users/jianhe/Documents/GitHub/Microservices-Project/monolithic-app/docker-compose.yml
     docker pull jh7939/monolithic:monolithic-app
-    docker-compose up
+    docker-compose up -d
     exit
 EOF
