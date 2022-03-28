252459a5aa854596bc9a6fd00f23dfcf6ef4d4c5
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-27 22:59:20 -0400

    1

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     echo $1
-    # IFS=', ' read -r TASKS <<< $1
+    IFS=', ' read -ra TASKS <<< $1
 
-    # for i in "${TASKS[@]}"; do
-    #     echo "$i"
-    #     echo "hello"
+    for i in "${TASKS[@]}"; do
+        echo $i
+        echo "hello"
 
     #     # cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     #     # security -v unlock-keychain -p "2446592ny" ~/Library/Keychains/login.keychain-db
     #     # docker pull jh7939/microservices:"$i"_microservice
     #     # bash update_containers.sh "$i" 4
-    # done
+    done
 EOF
