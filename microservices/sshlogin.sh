9e8eca445e0780a0bd69b1e2731d5b2f03fdc001
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-03-28 02:10:41 -0400

    1

--- microservices/sshlogin.sh
+++ microservices/sshlogin.sh
@@ -3,15 +3,5 @@ sshpass -p '2446592ny' ssh -T jianhe@192.168.2.13<< EOF
     cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
     security -v unlock-keychain -p "2446592ny" ~/Library/Keychains/login.keychain-db
     bash test.sh $1
-    # IFS=','  read -A FOLDERS <<< $1  
-    # for i in "$FOLDERS[@]"; do  
-    #     echo "$i"
-    #     echo "hello"
-    # done
-    # git fetch --all
-    # git checkout origin/master -- /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/docker-compose.yml
-    # docker pull jh7939/microservices:$1_microservice
-    # bash add_newservice.sh $1
-    # bash update_containers.sh $1 4
     exit
 EOF
--- microservices/test.sh
+++ microservices/test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
+cd /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/
+git fetch --all
+git checkout origin/master -- /Users/jianhe/Documents/GitHub/Microservices-Project/microservices/docker-compose.yml
 IFS=', '  read -ra FOLDERS <<< $1  
 for i in "${FOLDERS[@]}"; do  
-    echo "$i"
-    echo "hello"
+    docker pull jh7939/microservices:$1_microservice
+    bash add_newservice.sh $1
+    bash update_containers.sh $1 4
 done
